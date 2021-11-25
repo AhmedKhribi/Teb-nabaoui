748c54344a688c0b06dabdb65d2412cbfc62212b
Admin <Admin@DESKTOP-4J9FJ6R>
2021-11-25 17:00:06 +0100

    teb-nabaoui

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UnitComponent } from './unit/unit.component';
+import { UnitsComponent } from './units/units.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UnitComponent,
+    UnitsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
